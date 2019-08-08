300c0fcca1466a06b3d9b17bc10cd358c04835f8
shwetamali <53892753+shwetamali@users.noreply.github.com>
2019-08-08 12:28:46 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,8 @@
+ num1=input('First Number:')
+First Number:13.5
+ num2=input('Second Number:')
+Second Number:1.54
+>>> sum=float(num1)+float(num2)
+>>> print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
+The sum of 13.5 and 1.54 is 15.04
+>>>
