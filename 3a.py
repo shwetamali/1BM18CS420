1e18a1b8d0ad5591d6121768fc5cba1e1f2c2e43
shwetamali <53892753+shwetamali@users.noreply.github.com>
2019-09-05 11:29:45 +0530

    Create 3a.py

---
+++ 3a.py
@@ -0,0 +1,7 @@
+a=int(input("Enter an integers"))
+LRange=list(range(1,a+1))
+ans=[]
+for num in LRange:
+      if a%num==0:
+          ans.append(num)
+print("The divisor of that number:"+str(ans))
